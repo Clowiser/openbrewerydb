db466cc58c9b6d0df25bcecafe559fead30b3e13
Jessica <jessica.rigaud@le-campus-numerique.fr>
2021-10-14 18:37:31 +0200

    add test sur html/css/js

--- scripts.js
+++ scripts.js
@@ -13,6 +13,10 @@ function ready(callback) {
 
 function createDive(){
     const el = document.createElement('div');
-    el.innerHTML =  'Je suis un div';
+    el.setAttribute("id", "test");
+    // el.innerHTML =  'Je suis un div';
     document.body.appendChild(el);
+    $("#test").prepend("<p>WAAAAAAAAAAAAAAAAAANINIANAAAAAAA !!!</p>").css('background-color', 'white')
 }
+
+// $("test").css('background-color', 'white')
